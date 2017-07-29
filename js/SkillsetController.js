e59d0f6c5d3444d63d05b4aa3db47696bc25f1f2
Jake Demian <jakedemian@gmail.com>
2017-07-28 23:01:41 -0400

    remade skills section in angular

--- js/SkillsetController.js
+++ js/SkillsetController.js
@@ -1,3 +1,82 @@
-app.controller("SkillsetController", ["$scope", function($scope){
+app.controller("SkillsetController", ["$scope", function ($scope) {
   $scope.title = "Skillset";
+  $scope.skillsetCategories = [
+    {
+      "title": "Backend",
+      "skills": [
+        {
+          "name": "Java",
+          "level": 5
+        },
+        {
+          "name": "PHP",
+          "level": 4
+        },
+        {
+          "name": "C#",
+          "level": 4
+        },
+        {
+          "name": "C++",
+          "level": 3
+        }
+      ]
+    },
+    {
+      "title": "Frontend",
+      "skills": [
+        {
+          "name": "Javascript",
+          "level": 5
+        },
+        {
+          "name": "HTML",
+          "level": 5
+        },
+        {
+          "name": "CSS",
+          "level": 3
+        }
+      ]
+    },
+    {
+      "title": "Frameworks",
+      "skills": [
+        {
+          "name": "Angular",
+          "level": 4
+        },
+        {
+          "name": "jQuery",
+          "level": 4
+        },
+        {
+          "name": "SASS",
+          "level": 4
+        },
+        {
+          "name": "Struts",
+          "level": 4
+        },
+        {
+          "name": "React",
+          "level": 3
+        }
+      ]
+    },
+    {
+      "title": "Databases",
+      "skills": [
+        {
+          "name": "MySQL",
+          "level": 5
+        },
+        {
+          "name": "DB2",
+          "level": 5
+        }
+      ]
+    }
+  ];
+  
 }]);
